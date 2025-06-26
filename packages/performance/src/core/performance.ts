d397addd588104bf15a13ced31079310675cc147
18063323221 <1475929340@qq.com>
2025-06-26 17:21:25 +0800

    feat: 增加性能指标

--- packages/performance/src/core/performance.ts
+++ packages/performance/src/core/performance.ts
@@ -1,4 +1,102 @@
 import { Callback } from '@monitor/types';
+import { _global, on } from '@monitor/utils';
+import { onLCP, onFID, onCLS, onFCP, onTTFB } from 'web-vitals';
+
+let observer: MutationObserver;
+let firstScreenPaint = 0; // firstScreenPaint为首屏加载时间
+let isOnLoaded = false; // 页面是否渲染完成
+let entries: any[] = [];
+let timer: number;
+
+function getFirstScreenPaint(callback: Callback) {
+  if ('requestIdleCallback' in _global) {
+    requestIdleCallback(deadline => {
+      if (deadline.timeRemaining() > 0) {
+        observeFirstScreenPaint(callback);
+      }
+    });
+  } else {
+    observeFirstScreenPaint(callback);
+  }
+}
+
+//定时器循环监听dom的变化，当document.readyState === 'complete'时，停止监听。
+function checkDOMChange(callback: Callback) {
+  cancelAnimationFrame(timer);
+  timer = requestAnimationFrame(() => {
+    if (document.readyState === 'complete') {
+      isOnLoaded = true;
+    }
+    if (isOnLoaded) {
+      observer && observer.disconnect();
+      // document.readyState === 'complete'时，计算首屏渲染时间
+      firstScreenPaint = getRenderTime();
+      entries = [];
+      callback && callback(firstScreenPaint);
+    } else {
+      checkDOMChange(callback);
+    }
+  });
+}
+
+function getRenderTime(): number {
+  let startTime = 0;
+  entries.forEach(entry => {
+    if (entry.startTime > startTime) {
+      startTime = entry.startTime;
+    }
+  });
+  //  // performance.timing.navigationStart 页面的起始时间
+  return startTime - performance.timing.navigationStart;
+}
+
+const viewportWidth = _global.innerWidth;
+const viewportHeight = _global.innerHeight;
+function isInScreen(dom: HTMLElement): boolean {
+  const rectInfo = dom.getBoundingClientRect();
+  if (
+    rectInfo.top < viewportHeight && // 元素顶部在视口高度内
+    rectInfo.left < viewportWidth && // 元素左边在视口宽度内
+    rectInfo.bottom > 0 && // 元素底部在视口内（大于 0）
+    rectInfo.right > 0 // 元素右边在视口内（大于 0）
+  ) {
+    return true; // 元素部分或完全在屏幕内
+  }
+
+  return false; // 元素不在视口内
+}
+
+//外部通过callback拿到首屏加载时间
+export function observeFirstScreenPaint(callback: Callback): void {
+  const ignoreDOMList = ['STYLE', 'SCRIPT', 'LINK']; //这些标签在 DOM 变化时不会被处理。这是为了避免不必要的重新绘制或处理。
+  //MutationObserver 监听 DOM 变化：
+  observer = new MutationObserver((mutationList: any) => {
+    checkDOMChange(callback);
+    const entry = { children: [], startTime: 0 };
+    for (const mutation of mutationList) {
+      // 判断当前 mutation 是否包含新增节点，并且新增的节点是否在屏幕内
+      if (mutation.addedNodes.length && isInScreen(mutation.target)) {
+        for (const node of mutation.addedNodes) {
+          // 忽略 STYLE, SCRIPT, LINK 标签的变化
+          if (node.nodeType === 1 && !ignoreDOMList.includes(node.tagName) && isInScreen(node)) {
+            entry.children.push(node as never);
+          }
+        }
+      }
+    }
+    if (entry.children.length) {
+      entries.push(entry);
+      entry.startTime = new Date().getTime();
+    }
+  });
+
+  observer.observe(document, {
+    childList: true, // 监听添加或删除子节点
+    subtree: true, // 监听整个子树
+    characterData: true, // 监听元素的文本是否变化
+    attributes: true, // 监听元素的属性是否变化
+  });
+}
 
 /**
  * 是否为Safari浏览器
@@ -6,6 +104,27 @@ import { Callback } from '@monitor/types';
  */
 export function isSafari(): boolean {
   return /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
+}
+
+export function getResource(): PerformanceResourceTiming[] {
+  const entries = performance.getEntriesByType('resource');
+  // 过滤掉非静态资源的 fetch、 xmlhttprequest、beacon
+  let list = entries.filter(entry => {
+    return ['fetch', 'xmlhttprequest', 'beacon'].indexOf(entry.initiatorType) === -1;
+  });
+
+  if (list.length) {
+    list = JSON.parse(JSON.stringify(list));
+    list.forEach((entry: any) => {
+      entry.isCache = isCache(entry);
+    });
+  }
+  return list;
+}
+
+// 判断资源是否来自缓存
+export function isCache(entry: PerformanceResourceTiming): boolean {
+  return entry.transferSize === 0 || (entry.transferSize !== 0 && entry.encodedBodySize === 0);
 }
 
 //这个值通常用于衡量网页的交互响应性能，用户在页面上第一次交互的响应速度，直接影响用户体验。
@@ -116,6 +235,24 @@ export function getCLS(callback: Callback): void {
   const observer = new PerformanceObserver(entryHandler);
   observer.observe({ type: 'layout-shift', buffered: true });
 }
+//首字节时间,是衡量网页加载性能的一个重要指标，它表示从用户发出请求到浏览器收到第一个字节的数据所需要的时间。
+// 当用户访问一个网站时，整个加载过程可以分为几个阶段，首字节时间主要关注以下几个部分：
+// 请求发起：用户输入网址，浏览器发起请求，访问服务器。
+// DNS 解析：浏览器需要解析域名，获取对应的 IP 地址。
+// 建立连接：浏览器通过 TCP（对于 HTTPS 会有 TLS 握手）与服务器建立连接。
+// 服务器处理：服务器接收到请求并开始处理，直到开始返回数据。
+// 响应开始：服务器开始发送第一个字节的数据到浏览器，这个时刻就是 TTFB。
+export function getTTFB(callback: Callback): void {
+  on(_global, 'load', function () {
+    const { responseStart, navigationStart } = _global.performance.timing;
+    const value = responseStart - navigationStart;
+    callback({
+      name: 'TTFB',
+      value,
+      rating: value > 100 ? 'poor' : 'good',
+    });
+  });
+}
 
 export function getWebVitals(callback: Callback): void {
   // web-vitals 不兼容safari浏览器
@@ -132,5 +269,39 @@ export function getWebVitals(callback: Callback): void {
     getCLS(res => {
       callback(res);
     });
+    getTTFB(res => {
+      callback(res);
+    });
+  } else {
+    // 最大内容绘制时间:LCP 测量的是从页面开始加载到页面中最大的可视内容元素（如图片、视频、文字块等）被完全呈现所需的时间。
+    onLCP(res => {
+      callback(res);
+    });
+    //首次输入延迟:FID 测量的是用户首次与页面交互（例如点击按钮、链接或输入文本框）到浏览器响应该交互（即执行事件处理程序）之间的延迟时间。
+    onFID(res => {
+      callback(res);
+    });
+    //累积布局偏移：CLS 衡量的是页面加载过程中元素的视觉稳定性。它统计的是用户在页面加载过程中看到的所有意外布局偏移的程度。
+    onCLS(res => {
+      callback(res);
+    });
+    //(FCP) :首次内容绘制事件:即从页面开始加载到首次内容绘制发生的时间，
+    onFCP(res => {
+      callback(res);
+    });
+    ////首字节时间,是衡量网页加载性能的一个重要指标，它表示从用户发出请求到浏览器收到第一个字节的数据所需要的时间。
+    onTTFB(res => {
+      callback(res);
+    });
   }
+
+  //首屏加载时间
+  getFirstScreenPaint(res => {
+    const data = {
+      name: 'FSP',
+      value: res,
+      rating: res > 2500 ? 'poor' : 'good',
+    };
+    callback(data);
+  });
 }
--- packages/performance/src/index.ts
+++ packages/performance/src/index.ts
@@ -1,5 +1,7 @@
-import { EventTypes } from '@monitor/common';
+import { EventTypes, StatusCode } from '@monitor/common';
 import { BasePlugin, SdkBase } from '@monitor/types';
+import { getResource, getWebVitals } from './core/performance';
+import { _global, getTimestamp, on, setFlag } from '@monitor/utils';
 
 export default class WebPerformance extends BasePlugin {
   constructor() {
@@ -10,8 +12,56 @@ export default class WebPerformance extends BasePlugin {
 
   core({ transportData }: SdkBase) {
     // 获取FCP、LCP、TTFB、FID等指标
-    // getWebVitals(res:any) => {
-    // });
+    getWebVitals((res: any) => {
+      // name指标名称、rating 评级、value数值
+      const { name, rating, value } = res;
+      transportData.send({
+        type: EventTypes.PERFORMANCE,
+        status: StatusCode.OK,
+        time: getTimestamp(),
+        name,
+        rating,
+        value,
+      });
+    });
+
+    const observer = new PerformanceObserver(list => {
+      for (const long of list.getEntries()) {
+        transportData.send({
+          type: EventTypes.PERFORMANCE,
+          name: 'longtask',
+          longTask: long,
+          time: getTimestamp(),
+          status: StatusCode.OK,
+        });
+      }
+    });
+    observer.observe({ entryTypes: ['longtask'] });
+
+    on(_global, 'load', function () {
+      //上报资源列表
+      transportData.send({
+        type: EventTypes.PERFORMANCE,
+        name: 'resourceList',
+        time: getTimestamp(),
+        status: StatusCode.OK,
+        resourceList: getResource(),
+      });
+      //上报内存情况, safari、firefox不支持该属性
+      if (performance.memory) {
+        transportData.send({
+          type: EventTypes.PERFORMANCE,
+          name: 'memory',
+          time: getTimestamp(),
+          status: StatusCode.OK,
+          memory: {
+            jsHeapSizeLimit: performance.memory && performance.memory.jsHeapSizeLimit,
+            totalJSHeapSize: performance.memory && performance.memory.totalJSHeapSize,
+            usedJSHeapSize: performance.memory && performance.memory.usedJSHeapSize,
+          },
+        });
+      }
+    });
   }
   transform() {}
 }
